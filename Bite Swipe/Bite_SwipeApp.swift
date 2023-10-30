983ccc99b71fa1651597aa491a34fb23779cfd03
Ianrowl <iaro8747@colorado.edu>
2023-10-29 22:51:48 -0600

    Initial Commit

---
+++ Bite Swipe/Bite_SwipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bite_SwipeApp.swift
+//  Bite Swipe
+//
+//  Created by Ian Rowland on 10/29/23.
+//
+
+import SwiftUI
+
+@main
+struct Bite_SwipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
