9eb903455e285b575ef442feaa0bfb1aa3d602ac
Ianrowl <iaro8747@colorado.edu>
2023-11-09 17:26:41 -0700

    Full page "card", tap to switch to next one.

--- Bite Swipe/RestaurantView.swift
+++ Bite Swipe/RestaurantView.swift
@@ -7,44 +7,64 @@
 
 import SwiftUI
 
-
 struct RestaurantView: View {
     @ObservedObject var viewModel = RestaurantViewModel()
-        
+    @State private var currentIndex = 0
+    
     var body: some View {
-        List(viewModel.restaurants, id: \.name) { restaurant in
-            VStack(alignment: .leading) {
-                Text(restaurant.name)
-                    .font(.title)
-                    .padding(10)
-                    .multilineTextAlignment(.center)
-                    .frame(maxWidth: .infinity, alignment: .center)//Centers the text (ChatGPT assisted)
-                
-                Text("Cuisine: \(restaurant.cuisine)")
-                    .padding(10)
-                Text("Location: \(restaurant.location)")
-                    .padding(10)
-                
-                Spacer()
+        VStack {
+            if viewModel.restaurants.isEmpty {
+                ProgressView("Loading restaurants...")
+            } else {
+                RestaurantCard(restaurant: viewModel.restaurants[currentIndex])
+                    .onTapGesture {
+                        withAnimation {
+                            currentIndex = (currentIndex + 1) % viewModel.restaurants.count //Cycles continuously for now, will have it stop or reset in the future.
+                        }
+                    }
+                    .padding()
             }
-            .frame(maxWidth: .infinity)
-//            .frame(maxHeight: .infinity)
-            .background(Color.blue)
-            .cornerRadius(10)
-            .shadow(radius: 5)
-            .padding()
         }
+        .frame(maxHeight: .infinity)
+//        .background(Color.black)
         .onAppear {
             viewModel.fetchRestaurants()
-            }
         }
     }
+}
+
+
+struct RestaurantCard: View {
+    let moonGray = Color(white: 0.9, opacity: 0.7) //Found this color
+
+    let restaurant: Restaurant
+    
+    var body: some View {
+        VStack {
+            Text(restaurant.name)
+                .font(.largeTitle)
+                .multilineTextAlignment(.center)
+                .padding(50)
+            Text("Cuisine: \(restaurant.cuisine)")
+            Text("Location: \(restaurant.location)")
+            Spacer()
+        }
+        .frame(maxWidth: .infinity)
+        .background(moonGray)
+        .cornerRadius(10)
+        .shadow(radius: 5)
+    }
+}
+
 
 struct RestaurantView_Previews: PreviewProvider {
     static var previews: some View {
         RestaurantView()
     }
 }
+
+
+
 
 
 
